3d5e024a757a6abe9b3ed313b32490e381dd46c9
roblusk <130242918+roblusk@users.noreply.github.com>
2024-04-28 20:40:56 +0000

    Add authorization check for commenting on a photo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
+  #user can only comment on a photo if they follow the user or the user is public
+  before_action :is_an_authorized_user, only: [:destroy, :create]
 
   # GET /comments or /comments.json
   def index
@@ -66,5 +68,14 @@ class CommentsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def comment_params
       params.require(:comment).permit(:author_id, :photo_id, :body)
+    end
+
+    #does the user follow the photo owner or is the photo owner public?
+    #Recall "Owner" refers to photo owner not comment author
+    def is_an_authorized_user
+      @photo = Photo.find(params.fetch(:comment).fetch(:photo_id))
+      if current_user != @photo.owner && !@photo.owner.private? && !current_user.leaders.include(@photo.owner)
+        redirect_back fallback_location: root_url, alert: "Not authorized"
+      end
     end
 end
